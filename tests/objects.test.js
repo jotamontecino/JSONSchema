1fb112c63907ee57556711c6ddfad94ec4d56145
Javier Venegas <jota.montecino@gmail.com>
2019-10-28 16:40:30 +0100

    ADD: ajout tests d'objet simple

--- tests/objects.test.js
+++ tests/objects.test.js
@@ -42,7 +42,7 @@ const simpleSchema = {
   properties: {
     text: {
       type: "string",
-      pattern: "[a-zA-Z0-9 ]+",
+      pattern: "^[a-zA-Z0-9 ]+$",
       minLength: 5,
       maxLength: 20
     },
@@ -62,7 +62,7 @@ const simpleSchemaToJson = {
       type: 'string',
       description: '',
       defaultValue: null,
-      pattern: "[a-zA-Z0-9 ]+",
+      pattern: "^[a-zA-Z0-9 ]+$",
       minLength: 5,
       maxLength: 20
     },
@@ -82,18 +82,59 @@ const simpleSchemaToJson = {
 const simpleExemples = [
   {
     title: "Invalid type return an error",
-    result: ["Type should be boolean"],
+    result: ["The type should be Object"],
+    value: 9
+  },
+  {
+    title: "Invalid prop inside the body",
+    result: [{
+      "errors": [
+        "String doesn't validate for the pattern \"^[a-zA-Z0-9 ]+$\"",
+      ],
+      "path": "text",
+    }],
     value: {
       text: "j'ai Sauté",
       int: 9
     }
   },
   {
+    title: "Multiple errors inside one prop",
+    result: [{
+      "errors": [
+        "String should be at most \"20\" long.",
+        "String doesn't validate for the pattern \"^[a-zA-Z0-9 ]+$\"",
+      ],
+      "path": "text",
+    }],
+    value: {
+      text: "j'ai Sauté 12345678900987654321",
+      int: 9
+    }
+  },
+  {
+    title: "Multiple errors inside multiple props",
+    result: [ {
+        path: 'text',
+        errors: [ 'String should be at most "20" long.',
+           'String doesn\'t validate for the pattern "^[a-zA-Z0-9 ]+$"' ]
+      },
+      {
+        path: 'int',
+        errors: [ 'The integer value is below the defined strict minimum (8)' ]
+      }
+    ],
+    value: {
+      text: "j'ai Sauté 12345678900987654321",
+      int: 8
+    }
+  },
+  {
     title: "A good value shouldn't return errors",
     result: false,
     value: {
-      text: "jai Sautzqsffeq qsd sqd qsdfsd qsfqsd qdfqsd",
-      int: 3
+      text: "jai Sautzq0 9",
+      int: 12
     }
   }
 ];
